fe268ebe9789f97f55fc1df2cacc676ddb620310
Benjamin Obafemi <benjaminoba234@gmail.com>
2021-03-28 10:08:09 +0300

    update import components to import added files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
-import { QuoteVoteComponent } from './quote-vote/quote-vote.component';
 import { FormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HighlightHigestVoteDirective } from './highlight-higest-vote.directive';
 import { DateCountPipe } from './date-count.pipe';
+import { QuoteFooterComponent } from './quote-footer/quote-footer.component';
+import { QuoteHeaderComponent } from './quote-header/quote-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     QuoteFormComponent,
-    QuoteVoteComponent,
     HighlightHigestVoteDirective,
     DateCountPipe,
+    QuoteFooterComponent,
+    QuoteHeaderComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule, FontAwesomeModule],
   providers: [],
